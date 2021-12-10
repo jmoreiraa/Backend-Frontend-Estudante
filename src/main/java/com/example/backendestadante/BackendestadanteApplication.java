81fea8a3115602599910ea617307a65e263424df
jmoreiraa <jsds95@gmail.com>
2021-12-10 10:56:24 -0300

    Backend Part 1

---
+++ src/main/java/com/example/backendestadante/BackendestadanteApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendestadante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendestadanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendestadanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendestadante/BackendestadanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendestadante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendestadanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
